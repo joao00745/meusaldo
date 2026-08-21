b1ef472faa59db02be392ad269b3e360780278c8
joao00745 <joaoef583@gmail.com>
2026-08-21 14:14:57 -0300

    Delete MeuSaldo directory

--- MeuSaldo/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start(); //Inicia a sessão
-    session_unset(); //Apaga os dados da Sessão
-    session_destroy(); //Destroí a Sessão
-
-    header("Location: formLogin.php");
-    exit();
-?>
